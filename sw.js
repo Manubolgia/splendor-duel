6320fbc72b67419a048e939b3793b42d31e2f9a7
Manubolgia-Linux <manuel.obelleiro@bsc.es>
2026-06-12 15:21:26 +0200

    "bumped cache"

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Splendor Duel service worker: offline app shell + asset caching.
-const CACHE = 'splendor-duel-v2';
+const CACHE = 'splendor-duel-v3';
 const SHELL = ['./', './index.html', './engine.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
